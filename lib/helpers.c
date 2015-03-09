6a63b03d5f8b542dc22af23ba67a9de61b63f831
NoNick <aleksandrs634@gmail.com>
2015-03-09 22:59:09 +0000

    helpers are done

---
+++ lib/helpers.c
@@ -0,0 +1,27 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include "helpers.h"
+
+ssize_t read_(int fd, void *buf, size_t count) {
+    int curr;
+    for (curr = 0; curr < count; curr += n) {
+        int n = read(fd, buf + curr, count - curr);
+        if (n == -1)
+            return -1;
+        if (n == 0)
+            return 0;
+    }
+
+    return curr;
+}
+
+ssize_t write_(int fd, const void *buf, size_t count) {
+    int curr;
+    for (curr = 0; curr < count; curr += n) {
+        int n = write(fd, buf + curr, count - curr);
+        if (n == -1)
+            return -1;
+    }
+
+    return curr;
+}
