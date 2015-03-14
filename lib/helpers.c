eec29e15cefcf29640d8f548ae986f60d94975b3
NoNick <aleksandrs634@gmail.com>
2015-03-14 21:45:44 +0000

    fixes & read_until

--- lib/helpers.c
+++ lib/helpers.c
@@ -9,7 +9,7 @@ ssize_t read_(int fd, void *buf, size_t count) {
         if (n == -1)
             return -1;
         if (n == 0)
-            return 0;
+            return curr;
     }
 
     return curr;
@@ -18,9 +18,24 @@ ssize_t read_(int fd, void *buf, size_t count) {
 ssize_t write_(int fd, const void *buf, size_t count) {
     int curr, n;
     for (curr = 0; curr < count; curr += n) {
-        int n = write(fd, buf + curr, count - curr);
+        n = write(fd, buf + curr, count - curr);
         if (n == -1)
             return -1;
+    }
+
+    return curr;
+}
+
+ssize_t read_until(int fd, void * buf, size_t count, char delimiter) {
+    int curr, n;
+    for (curr = 0; curr < count; curr++) {
+        n = read(fd, buf + curr, 1);
+        if (n == -1)
+            return -1;
+        if (n == 0)
+            return curr;
+        if ((((char*)buf)[curr]) == delimiter)
+            return curr + 1;
     }
 
     return curr;
